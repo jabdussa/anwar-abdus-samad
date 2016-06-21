abe8c679d061b79e56534201a69ec5d6e3bfe7b0
Anwar Abdus-Samad <jabdussa@users.noreply.github.com>
2016-06-21 11:29:32 -0400

    PermsApi.scala
    Adding Perms API

---
+++ src/main/scala/api/PermsApi.scala
@@ -0,0 +1,31 @@
+package api
+
+import dao.PermDao
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import mappings.JsonMappings
+import models.Post
+import akka.http.scaladsl.server.Directives._
+import spray.json._
+
+trait PermsApi extends JsonMappings {
+
+  val postsApi =
+    (path("users"/IntNumber/"posts") & get){ userId =>
+      complete (PostsDao.findUserPosts(userId).map(_.toJson))
+    }~
+    (path("users"/IntNumber/"posts"/IntNumber) & get) { (userId,postId) =>
+      complete (PostsDao.findByUserIdAndId(userId, postId).map(_.toJson))
+    }~
+    (path("users"/IntNumber/"posts") & post) { userId => entity(as[Post]) { post =>
+      complete (PostsDao.create(post).map(_.toJson))
+    }}~
+    (path("users"/IntNumber/"posts"/IntNumber) & put) { (userId, id) => entity(as[Post]) { post =>
+      complete (PostsDao.update(post, id).map(_.toJson))
+    }}~
+    (path("users"/IntNumber/"posts"/IntNumber) & delete) { (userId, postId) =>
+      complete (PostsDao.delete(postId).map(_.toJson))
+    }
+
+}
