319aa551bdf3a90c46bce6aa6e7b927041ed6906
Anwar Abdus-Samad <jabdussa@gmail.com>
2016-07-04 11:35:45 -0400

    found scala dsl example

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,45 +3,36 @@ import org.apache.camel.builder.RouteBuilder
 
 import org.apache.camel.component.properties.PropertiesComponent
 
+import org.apache.camel.impl.DefaultPollingConsumerPollStrategy;
 
-class Main extends RouteBuilder {
-  
 
-def configure(): Unit =  {
+// we use a delay of 60 minutes (eg. once pr. hour we poll the FTP server
+long delay = 3600000;
 
-    // we use a delay of 60 minutes (eg. once pr. hour we poll the FTP server
-            long delay = 3600000;
+// from the given FTP server we poll (= download) all the files
+// from the public/reports folder as BINARY types and store this as files
+// in a local directory. Camel will use the filenames from the FTPServer
 
-            // from the given FTP server we poll (= download) all the files
-            // from the public/reports folder as BINARY types and store this as files
-            // in a local directory. Camel will use the filenames from the FTPServer
+// notice that the FTPConsumer properties must be prefixed with "consumer." in the URL
+// the delay parameter is from the FileConsumer component so we should use consumer.delay as
+// the URI parameter name. The FTP Component is an extension of the File Component.
 
-            // notice that the FTPConsumer properties must be prefixed with "consumer." in the URL
-            // the delay parameter is from the FileConsumer component so we should use consumer.delay as
-            // the URI parameter name. The FTP Component is an extension of the File Component.
+case class Creds(val host:String, val port:String, val user:String, val pass:String)
+case class Src(val path:String, val file: String)   
+case class Dst(val path:String, val file: String)
+      
+val creds = Creds("localhost", "22", "anwarabdus-samad", "") 
 
-            
-            case class Creds(val host:String, val port:String, val user:String, val pass:String)
-       
-            case class Src(val path:String, val file: String)   
-            case class Dst(val path:String, val file: String)
-            
-            val creds = Creds("localhost", "22", "anwarabdus-samad", "") 
-            
-            val srcPath = "/var/tmp/sftp_ingestion_test"
-            val srcFile = "data.txt"
-            
-            val dstPath = "/var/tmp/sftp_ingestion_test"
-            val dstFile = "ingestion.txt"
-            
-            // for the server we want to delay 5 seconds between polling the server
-            // and move downloaded files to a done sub directory
-            val sftpDelay   = "5s"
-            
-            val sftpMoveTo=done
-            
-            from(s"ftp://$user:$pass@$host$srcPath/$srcFile?binary=true&consumer&delay=$sftpDelay").to(s"file://$dstPath/$dstFile")
-            
-        }
-    };
-}
+val src = Src("/var/tmp/sftp_ingestion_test", "data.txt")
+
+val dst = Dst("/var/tmp/sftp_ingestion_test", "data.txt.mmddyyhhmmss")
+
+
+// for the server we want to delay 5 seconds between polling the server
+// and move downloaded files to a done sub directory
+val sftpDelay   = "5s"
+
+val sftpMoveTo  = "done"
+
+from(s"ftp://$creds.user:$creds.pass@$creds.host$src.path/$src.file?binary=true&consumer&delay=$sftpDelay").to(s"file://$dst.path/$dst.file")
+    
