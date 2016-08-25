1431203d1fb4fe292fa139ce1e27757e0ed864ee
Anwar Abdus-Samad <jabdussa@gmail.com>
2016-08-25 15:40:33 -0400

    updates

--- scripts/scala/git_checkout.scala
+++ scripts/scala/git_checkout.scala
@@ -1,4 +1,9 @@
 import sys.process._
+import java.io.File
+import java.util.Calendar
+import java.util.Date;
+import java.util.Locale;
+import java.text.SimpleDateFormat;
 
 
 val base_dir = "/var/tmp"
@@ -7,16 +12,25 @@ val repos = Array("individualSOR", "physicalProperty", "voyageSOR")
 
 val repo_url = "git clone ssh://git@github.com/gxitrident"
 
+val now = Calendar.getInstance().getTime()
+
+val fmt = new SimpleDateFormat("MM-dd-yyyy_hh:mm:ss", Locale.ENGLISH);
+
+val tstamp = fmt.format(now)
+
 for (r <- repos) {
 
-  val dir = new File(s"$base_dir/$r")
+  val old = new File(s"$base_dir/$r")
+  val cur = new File(s"$base_dir/$r.$tstamp")
 
-  if dir.  
+  if (old.exists) {
+    println(s"Backing Up: $old to $cur")
+    old.renameTo(cur)
+  }
 
   val cmd = s"$repo_url/$r.git"
 
-  println(s"cd $base_dir ; $cmd")
-
+  println(s"\ncd $base_dir ; $cmd")
   cmd!
 
 }
